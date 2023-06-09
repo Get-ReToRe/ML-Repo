a47fe246f640cbf39341ae50c2e6aeb68388af47
AldinWil10 <92682690+AldinWil10@users.noreply.github.com>
2023-06-09 21:08:40 +0700

    Update getretore_budget_recommendation.py

--- getretore_budget_recommendation.py
+++ getretore_budget_recommendation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import haversine_distances
 from math import radians
+import requests
 
 # Load data
 data_tourism = pd.read_csv('tourism_with_id.csv')
@@ -16,25 +17,31 @@ user_category = input("Masukkan kategori tempat wisata: ")
 # Filter data based on user input
 data_filtered = data_yogyakarta[(data_yogyakarta['Price'] <= user_budget) & (data_yogyakarta['Category'] == user_category)]
 
-# Calculate distances from Tugu Yogyakarta
+# Get current location
+response = requests.get('http://ip-api.com/json')
+data = response.json()
+latitude = data['lat']
+longitude = data['lon']
+
+# Calculate distances from current location
 def calculate_distance(lat, lon):
-    tugu_lat = radians(-7.7829)  # Tugu Yogyakarta latitude
-    tugu_lon = radians(110.3671)  # Tugu Yogyakarta longitude
+    current_lat = radians(latitude)  # Current location latitude
+    current_lon = radians(longitude)  # Current location longitude
     point_lat = radians(lat)
     point_lon = radians(lon)
-    distance = haversine_distances([[tugu_lat, tugu_lon], [point_lat, point_lon]])[1][0] * 6371  # Earth radius in kilometers
+    distance = haversine_distances([[current_lat, current_lon], [point_lat, point_lon]])[1][0] * 6371  # Earth radius in kilometers
     return distance
 
-data_filtered['distance_from_tugu'] = data_filtered.apply(lambda row: calculate_distance(row['Lat'], row['Long']), axis=1)
+data_filtered['distance_from_current'] = data_filtered.apply(lambda row: calculate_distance(row['Lat'], row['Long']), axis=1)
 
-# Sort by distance from Tugu Yogyakarta
-data_filtered = data_filtered.sort_values('distance_from_tugu')
+# Sort by distance from current location
+data_filtered = data_filtered.sort_values('distance_from_current')
 
 # Print recommendations
 print("Rekomendasi tempat wisata di Yogyakarta berdasarkan budget dan kategori:")
-for i, row in data_filtered.iterrows():
+for i, row in data_filtered.head(5).iterrows():
     print("Tempat Wisata:", row['Place_Name'])
     print("Kategori:", row['Category'])
     print("Harga Tiket:", row['Price'])
-    print("Estimasi Jarak dari Tugu Yogyakarta:", round(row['distance_from_tugu'], 2), "kilometer")
+    print("Estimasi Jarak dari Lokasi Terkini:", round(row['distance_from_current'], 2), "kilometer")
     print("---------")
